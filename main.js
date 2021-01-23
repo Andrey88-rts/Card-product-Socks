00de1e707e027d89e3085f1813b5aed7b2c6e5d0
Andrey <techpcb@mail.ru>
2021-01-24 01:37:03 +0400

    Реализован функционал изменения количества товаров в корзине и изменение изображения при наведении на название цвета

--- main.js
+++ main.js
@@ -2,11 +2,37 @@ Vue.createApp({
   data() {
     return {
       product: "Socks",
-      image: "./assets/vmSocks-blue-onWhite.jpg",
+      imageSrc: "./assets/vmSocks-green-onWhite.jpg",
       altText: "A pair of socks",
-      inStock: false,
-      inventory: 100,
-      onSale: true
+      inStock: true,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: 'green',
+          variantImage: "./assets/vmSocks-green-onWhite.jpg"
+        },
+        {
+          variantId: 2235,
+          variantColor: 'blue',
+          variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+        }
+      ],
+      cart: 0
     }
-  }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    subToCart() {
+      this.cart -= 1;
+      if (this.cart < 0) {
+        this.cart = 0;
+      }
+    },
+    changeImage(variantImage) {
+      this.imageSrc = variantImage;
+    }
+  },
 }).mount("#app")
