bbc35e7d8993de2a6802b2bc61c7366df544c387
Andrey <techpcb@mail.ru>
2021-01-24 23:35:23 +0400

    Реализованы вычисляемые свойства и компонентный подход, сформирована карточка товара

--- main.js
+++ main.js
@@ -1,4 +1,47 @@
-Vue.createApp({
+const app = Vue.createApp({
+
+});
+
+
+
+app.component('product', {
+
+  template: `
+  <div class="nav-bar">
+      <div class="cart">
+        <p>Cart({{ cart }})</p>
+      </div>
+    </div>
+    <div class="product">
+
+      <div class="product-image">
+        <img :src="imageSrc" />
+      </div>
+
+      <div class="product-info">
+        <h1>{{ product }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p :class="{outStock: !inStock}" v-else>Out Stock</p>
+        <p>Shipping: {{ shipping }}</p>
+        <ul>
+          <li v-for="detail in details">{{detail}}</li>
+        </ul>
+
+        <div class="wrap">
+          <div class="color-box" v-for="variant in variants" :key="variant.variantId"
+            :style="{backgroundColor: variant.variantColor}" @mouseover="changeImage(variant.variantImage)">
+
+          </div>
+        </div>
+
+        <button @click="addToCart" :class="{disabledButton: !inStock}">Add to cart</button>
+        <button @click="subToCart" :class="{disabledButton: !inStock || cart===0}">Sub to cart</button>
+
+
+      </div>
+
+    </div>
+  `,
   data() {
     return {
       product: "Socks",
@@ -35,4 +78,13 @@ Vue.createApp({
       this.imageSrc = variantImage;
     }
   },
-}).mount("#app")
+  computed: {
+    shipping() {
+      if (this.premium) {
+        return "Free";
+      } else {
+        return 2.99
+      }
+    }
+  }
+}).mount('#app')
